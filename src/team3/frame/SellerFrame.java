20f6afbca77a839a7a75467a072132ebf51ebc33
Evgenia <ievgenia.v@me.com>
2015-11-01 17:26:44 +0200

    просто тестую

---
+++ src/team3/frame/SellerFrame.java
@@ -0,0 +1,50 @@
+package team3.frame;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Evgenia on 11/1/15.
+ */
+public class SellerFrame extends JFrame {
+
+    private JTextArea fileContentArea;
+    private JButton scanButton;
+    private JButton billPaidButton;
+    private JButton eraseButton;
+    private JButton cancelButton;
+    private JButton signOutButton;
+
+    public SellerFrame(){
+        setSize(600,1000);
+        init();
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    private void init(){
+
+        JLabel listTitle = new JLabel();
+        JTextArea marketProductListArea = new JTextArea();
+
+        JButton scanButton = new JButton("Scan");
+        JButton billPaidButton = new JButton("Bill Paid");
+        JButton eraseButton = new JButton("Erase");
+        JButton cancelButton = new JButton("Cancel");
+        JButton signOutButton = new JButton("Sign out");
+
+        JPanel southButtonsPanel = new JPanel(new GridLayout(1,4));
+
+        southButtonsPanel.add(scanButton);
+        southButtonsPanel.add(billPaidButton);
+        southButtonsPanel.add(eraseButton);
+        southButtonsPanel.add(cancelButton);
+        southButtonsPanel.add(signOutButton);
+
+        getContentPane().add(listTitle, BorderLayout.NORTH);
+        getContentPane().add(marketProductListArea, BorderLayout.CENTER);
+        getContentPane().add(southButtonsPanel, BorderLayout.SOUTH);
+    }
+
+
+}
---
+++ src/team3/test_frame/TestSellerFrame.java
@@ -0,0 +1,14 @@
+package team3.test_frame;
+
+
+import team3.frame.SellerFrame;
+
+/**
+ * Created by Evgenia on 11/1/15.
+ */
+public class TestSellerFrame {
+
+    public static void main(String[] args) {
+        new SellerFrame();
+    }
+}
