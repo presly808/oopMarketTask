f63915eb1c5afa051f8b60576eeaf4610481bdd9
Maksim <maksim.sokur90@gmail.com>
2015-11-01 16:57:38 +0200

    Team3

--- src/team3/frame/LoginFrame.java
+++ src/team3/frame/LoginFrame.java
@@ -1,13 +1,31 @@
 package team3.frame;
 
+import javax.swing.*;
+
 /**
  * Created by 1 on 01.11.2015.
  */
-public class LoginFrame {
+public class LoginFrame extends JFrame{
 
+    private String name;
+    private JPasswordField passwordField;
 
+    public LoginFrame() {
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public JPasswordField getPasswordField() {
+        return passwordField;
+    }
 
+    public void setPasswordField(JPasswordField passwordField) {
+        this.passwordField = passwordField;
+    }
 }
