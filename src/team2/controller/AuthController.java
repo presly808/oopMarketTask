65b0521aa3107637a3a9b0dfd28f1d42f01c8288
Dima Virtuemaster <dima.kiev@gmail.com>
2015-11-01 16:46:00 +0200

    User authorization mockup

---
+++ src/team2/controller/AuthController.java
@@ -0,0 +1,17 @@
+package market.controller;
+
+import market.model.MarketDB;
+
+/**
+ * Created by Dima on 01.11.2015.
+ */
+public class AuthController {
+    private MarketDB marketDB;
+
+    public AuthController(MarketDB marketDB) {
+        this.marketDB = marketDB;
+    }
+
+
+}
+
