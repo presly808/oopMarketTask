454ab51e20394f8ebdf8d222e9220889fb0dac12
presly808 <presly808@gmail.com>
2015-11-01 17:09:56 +0200

    add two files, changed RunApp (just comment)

--- src/serhii/SerhiiClass.java
+++ src/serhii/SerhiiClass.java
@@ -4,4 +4,8 @@ package serhii;
  * Created by serhii on 01.11.15.
  */
 public class SerhiiClass {
+    // serhii comment
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
 }
