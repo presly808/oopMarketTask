cbfeeadc7e9624442229edd1a86d9def5b6c3eab
Dima Virtuemaster <dima.kiev@gmail.com>
2015-11-06 23:51:42 +0200

    v 0.5.1 adding new users ready

--- src/team2/view/AdministratorView.java
+++ src/team2/view/AdministratorView.java
@@ -15,6 +15,8 @@ public class AdministratorView extends JFrame{
 
     private AdminController adminController;
 
+    private JFrame frame = new JFrame("Administrator view");
+
     public AdministratorView(AdminController ac) {
         this.adminController = ac;
 
@@ -25,8 +27,9 @@ public class AdministratorView extends JFrame{
         System.out.println("now logged in: " + adminController.getMarketDB().getUser().toString());
         System.out.println("Admin view started!!!");
 
-        JFrame frame = new JFrame("Administrator view");
+        //JFrame frame = new JFrame("Administrator view");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        /// ???? ////// JFrame.setDefaultLookAndFeelDecorated(true);
 
         JMenuBar menuBar = new JMenuBar();
 
@@ -56,6 +59,12 @@ public class AdministratorView extends JFrame{
         ///////////
         JMenuItem addUser = new JMenuItem("Add user");
         usersMenu.add(addUser);
+        addUser.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                addUserMethod(frame);
+            }
+        });
+
         JMenuItem delUser = new JMenuItem("Remove user");
         usersMenu.add(delUser);
         JMenuItem showAllUser = new JMenuItem("Show all users");
@@ -79,7 +88,55 @@ public class AdministratorView extends JFrame{
         frame.setPreferredSize(new Dimension(800, 600));
         frame.pack();
         frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
 
+        frame.setVisible(true);
     }
+
+    private void addUserMethod(JFrame frame) {
+        JTextField login = new JTextField("",20);
+        JTextField password = new JTextField("",20);
+        JTextArea textArea = new JTextArea(10, 20);
+
+        JPanel content = new JPanel();
+        content.setLayout(new GridLayout(8,1));
+
+        content.add(new JLabel("Enter new user login: "));
+        content.add(login);
+        content.add(new JLabel("Enter new user password: "));
+        content.add(password);
+        content.add(new JLabel("Choose new user role: "));
+        String[] items = {"Seller", "Admin", "Guest"};
+        JComboBox roles = new JComboBox(items);
+        content.add(roles);
+
+        JButton createButton = new JButton("Create");
+        content.add(createButton);
+        createButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int newUserId = adminController.createUser(login.getText(), password.getText(), roles.getSelectedItem().toString());
+                String msg = "Unknown error.";
+                if ( newUserId == -5 ) {
+                    msg = "You try to make guest. One guest already registred in system.";
+                } else if ( newUserId == -1 ) {
+                    msg = "The login you enter is already registered";
+                } else if ( newUserId > 0 ) {
+                    msg = "User ID: " + newUserId + " login: " + login.getText() + " was registered successfully.";
+                }
+                String old = textArea.getText();
+                textArea.setText(old + "\n " + msg);
+            }
+        });
+
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        content.add(scrollPane);
+
+        frame.getContentPane().add(content);
+
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+
+        System.out.println("adding new user");
+    }
+
 }
